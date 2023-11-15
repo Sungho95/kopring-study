6bc5e394b1b7ab6336d1229cc698e9a239994e37
Sungho95 <>
2023-11-15 18:03:17 +0900

    initial commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     kotlin("jvm") version "1.8.22"
     kotlin("plugin.spring") version "1.8.22"
     kotlin("plugin.jpa") version "1.8.22"
+    kotlin("plugin.serialization") version "1.9.0"
 }
 
 group = "study"
@@ -24,7 +25,9 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+
     runtimeOnly("com.mysql:mysql-connector-j")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
--- src/main/kotlin/study/kopring/calculator/DateCalculator.kt
+++
@@ -1,9 +0,0 @@
-package study.kopring.calculator
-
-import java.time.LocalDate
-import java.time.Period
-
-
-fun ageCalculator(birthDate: LocalDate): Int {
-    return Period.between(birthDate, LocalDate.now()).years
-}
